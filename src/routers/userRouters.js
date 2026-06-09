15dfd71f06e12766ecb5ebf8634f9fae445a62f3
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2026-06-09 08:02:38 +0100

    errorbug

--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -20,7 +20,7 @@ router.get("/",async (req,res)=>{
         const users = await User.find()
         res.status(200).json(users)
     }catch(e){
-        res.status(500).json({message:error.message})
+        res.status(500).json({message:e.message})
     }
 })
 
